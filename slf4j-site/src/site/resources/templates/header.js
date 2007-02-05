162d2cab0f9167dd424c982fcd4dc23a21dda576
Sebastien Pennec <sebastien@qos.ch>
2007-02-05 14:10:22 +0000

    Updated the slf4j website to use our own templates + html files

---
+++ slf4j-site/src/site/resources/templates/header.js
@@ -0,0 +1,7 @@
+
+document.write('<p align="left">');
+document.write('<a href="http://www.slf4j.org/">');
+document.write('<img src="' + prefix + 'images/logos/slf4j-logo.jpg" alt="" border="0"/>');
+document.write('</a>')
+document.write('</p>');
+document.write('<div id="breadcrumbs"></div>');
