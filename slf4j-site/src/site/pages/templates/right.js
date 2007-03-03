8ef2a46405b1ba3c10daa95e887e207a745b67f8
Ceki Gulcu <ceki@qos.ch>
2007-03-03 21:10:24 +0000

    moving files from slf4j-site/src/site/resources to slf4j-site/src/site/pages

---
+++ slf4j-site/src/site/pages/templates/right.js
@@ -0,0 +1,22 @@
+
+document.write('<p class="menu_header">SLF4J-related buzz</p>');
+
+
+document.write('<p class="rm small"><a href="http://www.brokenbuild.com/blog/2007/02/21/maven2-cargo-and-deploying-to-jetty-6-with-commons-logging/">Maven2, Cargo etc.</p>');
+document.write('</p>');
+
+document.write('<p class="rm small"><a href="http://blog.organicelement.com/2006/12/21/commons-logging-classloader-woes/">JCL woes</a></p>');
+document.write('</p>');
+
+document.write('<p class="rm small"><a href="http://www.brodwall.com/johannes/blog/2007/01/20/article-ready-for-edit-embedded-web-integration-testing-with-jetty/">Embedded Web Integration</a>');
+document.write('</p>');
+
+
+document.write('<p class="rm small"><a href="http://www.fernandoribeiro.eti.br/2006/05/how-to-use-slf4j-with-log4j.html">How to Use SLF4J with log4j</a>');
+document.write('</p>');
+
+document.write('<p class="rm small"><a href="http://www.codesmell.de/blog/2006/03/cool-down-hot-deployment.html">Cool down the hot deployment</a>');
+document.write('</p>');
+
+document.write('<p class="rm small"><a href="http://sbtourist.blogspot.com/2007/01/join-us-be-commons-logging-free.html">Join us: Be Commons-Logging Free!</a>');
+document.write('</p>');
