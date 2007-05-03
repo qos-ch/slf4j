d4315c84d56d64b6134d4bccac343e7fcdb565ad
Ceki Gulcu <ceki@qos.ch>
2007-05-03 14:11:25 +0000

    added link to YASL and Mule. added link to netbeans marco blog by "deep blue"

--- slf4j-site/src/site/pages/templates/right.js
+++ slf4j-site/src/site/pages/templates/right.js
@@ -2,10 +2,14 @@
 document.write('<p class="menu_header">SLF4J-related buzz</p>');
 
 
-document.write('<p class="rm small"><a href="http://www.brokenbuild.com/blog/2007/02/21/maven2-cargo-and-deploying-to-jetty-6-with-commons-logging/">Maven2, Cargo etc.</p>');
+document.write('<p class="rm small"><a href="http://hohonuuli.blogspot.com/2007/04/sl4j-code-template-for-netbeans-i-use.html">Code Template for Netbeans</a>');
 document.write('</p>');
 
-document.write('<p class="rm small"><a href="http://blog.organicelement.com/2006/12/21/commons-logging-classloader-woes/">JCL woes</a></p>');
+
+document.write('<p class="rm small"><a href="http://www.brokenbuild.com/blog/2007/02/21/maven2-cargo-and-deploying-to-jetty-6-with-commons-logging/">Maven2, Cargo etc.</a>');
+document.write('</p>');
+
+document.write('<p class="rm small"><a href="http://blog.organicelement.com/2006/12/21/commons-logging-classloader-woes/">JCL woes</a>');
 document.write('</p>');
 
 document.write('<p class="rm small"><a href="http://www.brodwall.com/johannes/blog/2007/01/20/article-ready-for-edit-embedded-web-integration-testing-with-jetty/">Embedded Web Integration</a>');
