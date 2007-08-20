06866be44fcfd7b26c7d5289fc281cdb37aeb3ad
Ceki Gulcu <ceki@qos.ch>
2007-08-20 20:13:25 +0000

    - changes necessary for release 1.4.3

--- goVersion.sh
+++ goVersion.sh
@@ -3,4 +3,5 @@ VER=$1
 echo "Will use version $VER"
 
 find . -name "pom.xml" |grep -v archetype-resources|xargs perl version.pl $VER 
-perl version.pl $VER ./slf4j-site/src/site/site.xml
+
+#perl version.pl $VER ./slf4j-site/src/site/site.xml
