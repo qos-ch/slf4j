eb4f8e33e9a9081541ed52beed0a6bba56c84246
Ceki Gulcu <ceki@qos.ch>
2008-01-15 16:10:16 +0000

    - bind the MDC implementation in the jdk adapter to BasicMDCAdapter

--- slf4j-jdk14/src/main/java/org/slf4j/impl/StaticMDCBinder.java
+++ slf4j-jdk14/src/main/java/org/slf4j/impl/StaticMDCBinder.java
@@ -1,11 +1,11 @@
 package org.slf4j.impl;
 
-import org.slf4j.helpers.NOPMakerAdapter;
+import org.slf4j.helpers.BasicMDCAdapter;
 import org.slf4j.spi.MDCAdapter;
 
 
 /**
- * This implementation is bound to {@link NOPMakerAdapter}.
+ * This implementation is bound to {@link BasicMDCAdapter}.
  *
  * @author Ceki G&uuml;lc&uuml;
  */
@@ -22,13 +22,15 @@ public class StaticMDCBinder {
   
   /**
    * Currently this method always returns an instance of 
-   * {@link StaticMDCBinder}.
+   * {@link BasicMDCAdapter}.
    */
   public MDCAdapter getMDCA() {
-     return new NOPMakerAdapter();
+    // note that this method is invoked only from within the static initializer of 
+    // the org.slf4j.MDC class.
+    return new BasicMDCAdapter();
   }
   
   public String  getMDCAdapterClassStr() {
-    return NOPMakerAdapter.class.getName();
+    return BasicMDCAdapter.class.getName();
   }
 }
