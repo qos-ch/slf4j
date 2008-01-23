ab4cd5c277a81304fe2b9858fc227593eac4274b
Ceki Gulcu <ceki@qos.ch>
2008-01-23 20:30:54 +0000

    - force failre (for testing continuum)

--- slf4j-converter/src/test/java/org/slf4j/converter/ProjectConverterTest.java
+++ slf4j-converter/src/test/java/org/slf4j/converter/ProjectConverterTest.java
@@ -10,7 +10,7 @@ public class ProjectConverterTest extends TestCase {
   public void test() {
   }
   
-  public void XtestBarracuda() {
+  public void testBarracuda() {
     ProjectConverter pc = new ProjectConverter(Constant.LOG4J_TO_SLF4J);
     File projectFolder = new File("c:/home/ceki//Varia/Barracuda");
     pc.convertProject(projectFolder);
