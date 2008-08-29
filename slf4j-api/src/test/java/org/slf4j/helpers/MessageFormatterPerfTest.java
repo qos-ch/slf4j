e0b6adc94cb75c17267f370081339af0f527dc36
Ceki Gulcu <ceki@qos.ch>
2008-08-29 15:14:00 +0000

    - use BogoPerf utility. - Using System.currentMillis instead of System.nanoTime. The latter is only available in   JDK 1.5

--- slf4j-api/src/test/java/org/slf4j/helpers/MessageFormatterPerfTest.java
+++ slf4j-api/src/test/java/org/slf4j/helpers/MessageFormatterPerfTest.java
@@ -22,38 +22,37 @@ public class MessageFormatterPerfTest extends TestCase {
 
   public void XtestJDKFormatterPerf() {
     jdkMessageFormatter(RUN_LENGTH);
-    double avg = jdkMessageFormatter(RUN_LENGTH);
-    System.out.println("jdk avg = "+avg+" nanos");
+    double duration = jdkMessageFormatter(RUN_LENGTH);
+    System.out.println("jdk duration = "+duration+" nanos");
   }
   
   public void testSLF4JPerf() {
     slf4jMessageFormatter(RUN_LENGTH);
-    double avgDuration = slf4jMessageFormatter(RUN_LENGTH);
-    
-    long referencePerf = 1700;
-    BogoPerf.assertDuration(avgDuration, referencePerf, REFERENCE_BIPS);
+    double duration = slf4jMessageFormatter(RUN_LENGTH);
+    long referencePerf = 140;
+    BogoPerf.assertDuration(duration, referencePerf, REFERENCE_BIPS);
   }
 
   public double slf4jMessageFormatter(long len) {
     String s = ""; 
     s += ""; // keep compiler happy
-    long start = System.nanoTime();
+    long start = System.currentTimeMillis();
     for (int i = 0; i < len; i++) {
       s = MessageFormatter.format("This is some rather short message {} ", i1);
     }
-    long end = System.nanoTime();
-    return (1.0*end - start) / len;
+    long end = System.currentTimeMillis();
+    return (1.0*end - start);
   }  
   public double jdkMessageFormatter(long len) {
     String s = ""; 
     s += ""; // keep compiler happy
-    long start = System.nanoTime();
+    long start = System.currentTimeMillis();
     Object[] oa = new Object[] {i1};
     for (int i = 0; i < len; i++) {
       s = MessageFormat.format("This is some rather short message {0}", oa);
     }
-    long end = System.nanoTime();
-    return (1.0*end - start) / len;
+    long end = System.currentTimeMillis();
+    return (1.0*end - start);
   }
 
 }
