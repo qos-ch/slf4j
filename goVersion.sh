005a6338f6c21b4012f0005cf292d1a1bd2bd9f8
Ceki Gulcu <ceki@qos.ch>
2008-10-02 15:24:28 +0000

    - adding VERSION related compatibility checks (code to follow)

--- goVersion.sh
+++ goVersion.sh
@@ -1,7 +1,6 @@
-
 VER=$1
 echo "Will use version $VER"
-
+echo "Changing pom.xml files"
 find . -name "pom.xml" |grep -v archetype-resources|xargs perl version.pl $VER 
-
-#perl version.pl $VER ./slf4j-site/src/site/site.xml
+echo "Changing Java files"
+find . -name "StaticLoggerBinder.java" |grep -v archetype-resources|xargs perl binderVersion.pl $VER
