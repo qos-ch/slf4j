68af3bbdbb882da5dc4bc75bbf945acc19ce54fd
Ceki Gulcu <ceki@qos.ch>
2009-09-23 11:57:04 +0200

    - started work on version 1.5.10 - fixed bug 151 http://bugzilla.slf4j.org/show_bug.cgi?id=151

--- slf4j-api/src/main/java/org/slf4j/impl/StaticLoggerBinder.java
+++ slf4j-api/src/main/java/org/slf4j/impl/StaticLoggerBinder.java
@@ -61,7 +61,7 @@ public class StaticLoggerBinder {
    * The value of this field is usually modified with each release. 
    */
   // to avoid constant folding by the compiler, this field must *not* be final
-  public static String REQUESTED_API_VERSION = "1.5.9-RC0";  // !final
+  public static String REQUESTED_API_VERSION = "1.5.10";  // !final
   
   private StaticLoggerBinder() {
     throw new UnsupportedOperationException("This code should have never made it into the jar");
