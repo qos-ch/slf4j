bc8b8f8eae7b16ff0227165e6e477f983f0f27eb
Ceki Gulcu <ceki@qos.ch>
2010-03-19 09:40:14 +0100

    - added popup asking users to subscribe to the announce mailing list - edits to the faq

--- slf4j-site/src/site/pages/templates/left.js
+++ slf4j-site/src/site/pages/templates/left.js
@@ -31,8 +31,13 @@ document.write('  </p>');
 document.write('</div>');
 
 document.write('<p>&nbsp;</p>');
+document.write('<p class="survey">Take our <a href="http://ceki.questionform.com/public/slf4j">two question survey</a>.</p>');
+
+document.write('<p>&nbsp;</p>');
 document.write('<p class="pub">');
 document.write('  <a href="https://www.qos.ch/shop/products/log4jManual">');
 document.write('    <img src="images/buyDirect.jpg" border="0" title="" alt="buy direct from the developer"/>');
 document.write('  </a>');
 document.write('</p>');
+
+
