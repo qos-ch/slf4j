bdc80857eea22530e29222fd773e847c44e7ce3e
Ceki Gulcu <ceki@qos.ch>
2011-04-27 22:06:51 +0200

    removing the job link

--- slf4j-site/src/site/pages/templates/header.js
+++ slf4j-site/src/site/pages/templates/header.js
@@ -4,9 +4,9 @@ document.write('<td><a href="http://www.slf4j.org/">');
 document.write('<img src="' + prefix + 'images/logos/slf4j-logo.jpg" alt="" border="0"/>');
 document.write('</a></td>')
 
-document.write('<td align="right"><a id="job" href="http://logback.qos.ch/job.html">');
-document.write('<img src="' + prefix + 'images/myjob.png" alt="" border="0"/>');
-document.write('</a></td>')
+//document.write('<td align="right"><a id="job" href="http://logback.qos.ch/job.html">');
+//document.write('<img src="' + prefix + 'images/myjob.png" alt="" border="0"/>');
+//document.write('</a></td>')
 
 document.write('</tr></table>');
 document.write('<div id="breadcrumbs"></div>');
