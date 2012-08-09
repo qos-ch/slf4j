c2f277a5357901e6856bec38a41ecde8c37dce0b
mbishop <matthew.bishop@elasticpath.com>
2012-08-09 11:24:32 -0700

    Bugzilla #269.

--- osgi-over-slf4j/src/main/java/org/slf4j/osgi/logservice/impl/Activator.java
+++ osgi-over-slf4j/src/main/java/org/slf4j/osgi/logservice/impl/Activator.java
@@ -1,8 +1,8 @@
-/* 
+/*
  * Copyright (c) 2004-2005 QOS.ch
- * 
+ *
  * All rights reserved.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
  * "Software"), to  deal in  the Software without  restriction, including
@@ -12,7 +12,7 @@
  * copyright notice(s) and this permission notice appear in all copies of
  * the  Software and  that both  the above  copyright notice(s)  and this
  * permission notice appear in supporting documentation.
- * 
+ *
  * THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
  * EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR  A PARTICULAR PURPOSE AND NONINFRINGEMENT
@@ -22,7 +22,7 @@
  * RESULTING FROM LOSS  OF USE, DATA OR PROFITS, WHETHER  IN AN ACTION OF
  * CONTRACT, NEGLIGENCE  OR OTHER TORTIOUS  ACTION, ARISING OUT OF  OR IN
  * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- * 
+ *
  * Except as  contained in  this notice, the  name of a  copyright holder
  * shall not be used in advertising or otherwise to promote the sale, use
  * or other dealings in this Software without prior written authorization
@@ -32,7 +32,7 @@
 
 package org.slf4j.osgi.logservice.impl;
 
-import java.util.Hashtable;
+import java.util.Properties;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -40,38 +40,35 @@ import org.osgi.framework.ServiceFactory;
 import org.osgi.service.log.LogService;
 
 /**
- * <code>Activator</code> implements a simple bundle that registers a 
+ * <code>Activator</code> implements a simple bundle that registers a
  * {@link LogServiceFactory} for the creation of {@link LogService} implementations.
 **/
 public class Activator implements BundleActivator {
-
     /**
-     * 
-	 * Implements <code>BundleActivator.start()</code> to register a 
+     *
+	 * Implements <code>BundleActivator.start()</code> to register a
      * LogServiceFactory.
      *
      * @param bundleContext the framework context for the bundle
      * @throws Exception
      */
     public void start(BundleContext bundleContext) throws Exception {
-        Hashtable props = new Hashtable();
+        Properties props = new Properties();
         props.put("description", "An slf4j implementation.");
         ServiceFactory factory = new LogServiceFactory();
-        bundleContext.registerService(LogService.class.getName(), factory, props); 
+        bundleContext.registerService(LogService.class.getName(), factory, props);
     }
 
     /**
-     * 
-     * Implements <code>BundleActivator.stop()</code>. 
-     * 
+     *
+     * Implements <code>BundleActivator.stop()</code>.
+     *
      * @param bundleContext the framework context for the bundle
      * @throws Exception
      */
     public void stop(BundleContext bundleContext) throws Exception {
-       
+
         // Note: It is not required that we remove the service here, since
         // the framework will do it automatically anyway.
     }
-
-    
 }
