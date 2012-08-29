993e6b34079ee96cf188e966ed2fd1f78f4be3d0
Ceki Gulcu <ceki@qos.ch>
2012-08-29 15:10:47 +0200

    remove link to ohloh counter

--- slf4j-site/src/site/pages/templates/footer.js
+++ slf4j-site/src/site/pages/templates/footer.js
@@ -4,10 +4,6 @@ document.write('<table class="footer" border="0">')
 document.write('<tr>')
 
 document.write('  <td valign="top" align="left">Copyright &copy; 2004-2012  <a href="http://www.qos.ch/">QOS.ch</a></td>')
-
-document.write('  <td align="right" rowspan="2">')
-document.write('    <script type="text/javascript" src="http://www.ohloh.net/p/11217/widgets/project_users.js"></script>')
-document.write('  </td>')
 document.write('</tr>')
 
 AAT = '@'
@@ -16,7 +12,6 @@ document.write('<tr>')
 document.write('<td align="left" colspan="1">')
 document.write('We are actively looking for volunteers to proofread the documentation. Please send your corrections or suggestions for improvement to "corrections' + AAT +'qos'+DOOTT+'ch". See also the <a href="http://articles.qos.ch/contributing.html">instructions for contributors</a>.');
 document.write('</td>')
-document.write('  <td>&nbsp;</td>')
 document.write('</tr>')
 
 
