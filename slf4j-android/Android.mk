41353a528cc139bad64a9c34d434a17a46a56ff5
mar-v-in <github@rvin.mooo.com>
2014-03-25 18:38:32 +0100

    Android.mk only for slf4j-android

--- slf4j-android/Android.mk
+++ slf4j-android/Android.mk
@@ -2,7 +2,11 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := slf4j-android
-LOCAL_STATIC_JAVA_LIBRARIES := slf4j-api
-LOCAL_SRC_FILES := $(call all-java-files-under, src/main/java)
+
+api_src := $(call all-java-files-under, ../slf4j-api/src/main/java)
+api_impl := $(call all-java-files-under, ../slf4j-api/src/main/java/org/slf4j/impl)
+api_src_no_impl := $(filter-out $(api_impl), $(api_src))
+
+LOCAL_SRC_FILES := $(api_src_no_impl) $(call all-java-files-under, src/main/java)
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
