5c76844e4cd56292f46cabb3f0cfcc39f50253a2
William Delanoue <william.delanoue@deveryware.net>
2014-07-25 14:21:30 +0200

    add MDCCloseable

--- slf4j-api/src/main/java/org/slf4j/MDC.java
+++ slf4j-api/src/main/java/org/slf4j/MDC.java
@@ -24,6 +24,8 @@
  */
 package org.slf4j;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.util.Map;
 
 import org.slf4j.helpers.NOPMDCAdapter;
@@ -65,6 +67,21 @@ public class MDC {
   static final String NULL_MDCA_URL = "http://www.slf4j.org/codes.html#null_MDCA";
   static final String NO_STATIC_MDC_BINDER_URL = "http://www.slf4j.org/codes.html#no_static_mdc_binder";
   static MDCAdapter mdcAdapter;
+
+  /**
+   * An adapter to remove the key when done.
+   */
+  private static class MDCCloseable implements Closeable {
+    private final String key;
+
+    private MDCCloseable(String key) {
+      this.key = key;
+    }
+
+    public void close() {
+      MDC.remove(this.key);
+    }
+  }
 
   private MDC() {
   }
@@ -114,6 +131,40 @@ public class MDC {
           + NULL_MDCA_URL);
     }
     mdcAdapter.put(key, val);
+  }
+
+  /**
+   * Put a diagnostic context value (the <code>val</code> parameter) as identified with the
+   * <code>key</code> parameter into the current thread's diagnostic context map. The
+   * <code>key</code> parameter cannot be null. The <code>val</code> parameter
+   * can be null only if the underlying implementation supports it.
+   *
+   * <p>
+   * This method delegates all work to the MDC of the underlying logging system.
+   * <p>
+   * This method return a <code>Closeable</code> object who can remove <code>key</code> when
+   * <code>close</code> is called.
+   *
+   * <p>
+   * Useful with Java 7 for example :
+   * <code>
+   *   try(Closeable closeable = MDC.putCloseable(key, value)) {
+   *     ....
+   *   }
+   * </code>
+   *
+   * @param key non-null key
+   * @param val value to put in the map
+   * @return a <code>Closeable</code> who can remove <code>key</code> when <code>close</code>
+   * is called.
+   *
+   * @throws IllegalArgumentException
+   *           in case the "key" parameter is null
+   */
+  public static Closeable putCloseable(String key, String val)
+    throws IllegalArgumentException {
+    put(key, val);
+    return new MDCCloseable(key);
   }
 
   /**
