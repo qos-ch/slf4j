ecf7c14ef10e45daa198dcba9b01f1d77909a6d6
Marcos Scriven <mscriven@atlassian.com>
2014-11-21 11:23:58 +1100

    Upgraded from Commons Logging 1.1.1 to 1.1.3. Commons Logging classes (incl. licences) copied from git@github.com:apache/commons-logging.git at tag LOGGING_1_1_3 (f8b79c7)

--- jcl-over-slf4j/src/main/java/org/apache/commons/logging/impl/NoOpLog.java
+++ jcl-over-slf4j/src/main/java/org/apache/commons/logging/impl/NoOpLog.java
@@ -1,9 +1,10 @@
 /*
- * Copyright 2001-2004 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -20,126 +21,84 @@ import java.io.Serializable;
 import org.apache.commons.logging.Log;
 
 /**
- * <p>
- * Trivial implementation of Log that throws away all messages. No configurable
- * system properties are supported.
- * </p>
- * 
- * @author <a href="mailto:sanders@apache.org">Scott Sanders</a>
- * @author Rod Waldhoff
- * @version $Id: NoOpLog.java,v 1.8 2004/06/06 21:13:12 rdonkin Exp $
+ * Trivial implementation of Log that throws away all messages.  No
+ * configurable system properties are supported.
+ *
+ * @version $Id$
  */
 public class NoOpLog implements Log, Serializable {
-  private static final long serialVersionUID = 561423906191706148L;
 
-  /** Convenience constructor */
-  public NoOpLog() {
-  }
+    /** Serializable version identifier. */
+    private static final long serialVersionUID = 561423906191706148L;
 
-  /** Base constructor */
-  public NoOpLog(String name) {
-  }
+    /** Convenience constructor */
+    public NoOpLog() { }
+    /** Base constructor */
+    public NoOpLog(String name) { }
+    /** Do nothing */
+    public void trace(Object message) { }
+    /** Do nothing */
+    public void trace(Object message, Throwable t) { }
+    /** Do nothing */
+    public void debug(Object message) { }
+    /** Do nothing */
+    public void debug(Object message, Throwable t) { }
+    /** Do nothing */
+    public void info(Object message) { }
+    /** Do nothing */
+    public void info(Object message, Throwable t) { }
+    /** Do nothing */
+    public void warn(Object message) { }
+    /** Do nothing */
+    public void warn(Object message, Throwable t) { }
+    /** Do nothing */
+    public void error(Object message) { }
+    /** Do nothing */
+    public void error(Object message, Throwable t) { }
+    /** Do nothing */
+    public void fatal(Object message) { }
+    /** Do nothing */
+    public void fatal(Object message, Throwable t) { }
 
-  /** Do nothing */
-  public void trace(Object message) {
-  }
+    /**
+     * Debug is never enabled.
+     *
+     * @return false
+     */
+    public final boolean isDebugEnabled() { return false; }
 
-  /** Do nothing */
-  public void trace(Object message, Throwable t) {
-  }
+    /**
+     * Error is never enabled.
+     *
+     * @return false
+     */
+    public final boolean isErrorEnabled() { return false; }
 
-  /** Do nothing */
-  public void debug(Object message) {
-  }
+    /**
+     * Fatal is never enabled.
+     *
+     * @return false
+     */
+    public final boolean isFatalEnabled() { return false; }
 
-  /** Do nothing */
-  public void debug(Object message, Throwable t) {
-  }
+    /**
+     * Info is never enabled.
+     *
+     * @return false
+     */
+    public final boolean isInfoEnabled() { return false; }
 
-  /** Do nothing */
-  public void info(Object message) {
-  }
+    /**
+     * Trace is never enabled.
+     *
+     * @return false
+     */
+    public final boolean isTraceEnabled() { return false; }
 
-  /** Do nothing */
-  public void info(Object message, Throwable t) {
-  }
-
-  /** Do nothing */
-  public void warn(Object message) {
-  }
-
-  /** Do nothing */
-  public void warn(Object message, Throwable t) {
-  }
-
-  /** Do nothing */
-  public void error(Object message) {
-  }
-
-  /** Do nothing */
-  public void error(Object message, Throwable t) {
-  }
-
-  /** Do nothing */
-  public void fatal(Object message) {
-  }
-
-  /** Do nothing */
-  public void fatal(Object message, Throwable t) {
-  }
-
-  /**
-   * Debug is never enabled.
-   * 
-   * @return false
-   */
-  public final boolean isDebugEnabled() {
-    return false;
-  }
-
-  /**
-   * Error is never enabled.
-   * 
-   * @return false
-   */
-  public final boolean isErrorEnabled() {
-    return false;
-  }
-
-  /**
-   * Fatal is never enabled.
-   * 
-   * @return false
-   */
-  public final boolean isFatalEnabled() {
-    return false;
-  }
-
-  /**
-   * Info is never enabled.
-   * 
-   * @return false
-   */
-  public final boolean isInfoEnabled() {
-    return false;
-  }
-
-  /**
-   * Trace is never enabled.
-   * 
-   * @return false
-   */
-  public final boolean isTraceEnabled() {
-    return false;
-  }
-
-  /**
-   * Warn is never enabled.
-   * 
-   * @return false
-   */
-  public final boolean isWarnEnabled() {
-    return false;
-  }
-
+    /**
+     * Warn is never enabled.
+     *
+     * @return false
+     */
+    public final boolean isWarnEnabled() { return false; }
 }
