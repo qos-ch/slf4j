22c00b7b886ddd5b5228aa30546b5ccdbd095f67
Alexander Dorokhine <alexander@dorokhine.com>
2014-12-06 14:33:22 -0800

    A field trial for the automatically named logger.
    Can be enabled with -Dorg.slf4j.LoggerFactory.autoNamedLoggerFieldTrial=true.
    
    Prints an error (and stacktrace) to stderr when a mismatch between custom and automatic logger names is detected.

--- slf4j-api/src/main/java/org/slf4j/helpers/Util.java
+++ slf4j-api/src/main/java/org/slf4j/helpers/Util.java
@@ -31,8 +31,48 @@ package org.slf4j.helpers;
  *
  * @author Ceki G&uuml;lc&uuml;
  */
-public class Util {
-    
+public final class Util {
+
+  private Util() {}
+
+  /**
+   * In order to call {@link SecurityManager#getClassContext()}, which is a
+   * protected method, we add this wrapper which allows the method to be visible
+   * inside this package.
+   */
+  private static final class ClassContextSecurityManager extends SecurityManager {
+    protected Class<?>[] getClassContext() {
+      return super.getClassContext();
+    }
+  };
+  private static final ClassContextSecurityManager SECURITY_MANAGER =
+      new ClassContextSecurityManager();
+
+  /**
+   * Returns the name of the class which called the invoking method.
+   * @return the name of the class which called the invoking method.
+   */
+  public static String getCallingClassName() {
+    Class<?>[] trace = SECURITY_MANAGER.getClassContext();
+    String thisClassName = Util.class.getName();
+
+    // Advance until Util is found
+    int i;
+    for (i = 0 ; i < trace.length ; i++) {
+      if (thisClassName.equals(trace[i].getName()))
+        break;
+    }
+
+    // trace[i] = Util; trace[i+1] = caller; trace[i+2] = caller's caller
+    if (i >= trace.length || i + 2 >= trace.length) {
+      throw new IllegalStateException(
+          "Failed to find org.slf4j.helpers.Util or its caller in the stack; " +
+          "this should not happen");
+    }
+
+    return trace[i+2].getName();
+  }
+
   static final public void report(String msg, Throwable t) {
     System.err.println(msg);
     System.err.println("Reported exception:");
