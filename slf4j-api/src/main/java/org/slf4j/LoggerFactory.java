8a1702ba0f1ccd5dec8b4fca59829399313edeac
Ceki Gulcu <ceki@qos.ch>
2014-12-14 17:51:48 +0100

    polish the docs on slf4j.detectLoggerNameMismatch

--- slf4j-api/src/main/java/org/slf4j/LoggerFactory.java
+++ slf4j-api/src/main/java/org/slf4j/LoggerFactory.java
@@ -292,8 +292,17 @@ public final class LoggerFactory {
    * Return a logger named corresponding to the class passed as parameter, using
    * the statically bound {@link ILoggerFactory} instance.
    *
+   * <p>In case the the <code>clazz</code> parameter differs from the name of
+   * the caller as computed internally by SLF4J, a logger name mismatch warning will be 
+   * printed but only if the <code>slf4j.detectLoggerNameMismatch</code> system property is 
+   * set to true. By default, this property is not set and no warnings will be printed
+   * even in case of a logger name mismatch.
+   * 
    * @param clazz the returned logger will be named after clazz
    * @return logger
+   *
+   *
+   * @see <a href="http://www.slf4j.org/codes.html#loggerNameMismatch">Detected logger name mismatch</a> 
    */
   @Nonnull
   public static Logger getLogger(@Nonnull Class<?> clazz) {
