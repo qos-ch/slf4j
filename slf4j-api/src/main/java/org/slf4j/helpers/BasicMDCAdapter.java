95a6f46ebae7e39b94bd0323c5359df427bcb045
Luke Cwik <lcwik@google.com>
2015-04-07 15:37:31 -0700

    Removed Google Copyright.

--- slf4j-api/src/main/java/org/slf4j/helpers/BasicMDCAdapter.java
+++ slf4j-api/src/main/java/org/slf4j/helpers/BasicMDCAdapter.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2004-2011 QOS.ch, Copyright (C) 2015 Google Inc.
+ * Copyright (c) 2004-2011 QOS.ch
  * All rights reserved.
  *
  * Permission is hereby granted, free  of charge, to any person obtaining
