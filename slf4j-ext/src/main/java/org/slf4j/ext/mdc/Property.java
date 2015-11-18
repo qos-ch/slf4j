a6dae90a7565bad0085c55a2e980b1192437a596
Himanshu Vijay <himanshu.vijay@edifecs.com>
2015-11-17 19:53:48 -0800

    Added TODO for annotation processing check.

--- slf4j-ext/src/main/java/org/slf4j/ext/mdc/Property.java
+++ slf4j-ext/src/main/java/org/slf4j/ext/mdc/Property.java
@@ -7,14 +7,16 @@ import java.lang.annotation.Target;
 
 /**
  * Used to annotate property of a @Pojo.
- *
+ * <p/>
  * Created by himavija on 10/9/15.
  * TODO: Annotation processor should check that this can only by applied to primitive types, list, set, map, jsonobjects.
- *       If not then to POJOs that themselves have child @property members.
+ *        If not then to POJOs that themselves have child @property members.
+ * TODO: defaultValue. The class type of defaultValue should be same as type of the field on which this annotation is applied.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Property {
-    boolean dontLogIfDefault() default false;
-    String name();
+  boolean dontLogIfDefault() default false;
+
+  String name();
 }
