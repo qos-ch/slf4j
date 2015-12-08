4ad53bc84d825d252b2b2a96658d546031336d68
mfarid <farid.iflex@gmail.com>
2015-12-08 19:28:11 +0530

    UTS-131 Added test cases for log4j module

--- slf4j-log4j12/src/test/java/org/slf4j/impl/Log4jMDCAdapterTest.java
+++ slf4j-log4j12/src/test/java/org/slf4j/impl/Log4jMDCAdapterTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -12,6 +13,8 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({MDC.class})
@@ -22,10 +25,11 @@ public class Log4jMDCAdapterTest {
   @Before
   public void setup(){
     log4jMDCAdapter = new Log4jMDCAdapter();
+    PowerMockito.mockStatic(MDC.class);
   }
-  
+
   @Test
-  public void testGetCopyOfContextMap() throws Exception {
+  public void testGetCopyOfContextMap_withContextNotNull() throws Exception {
     //setup
     PowerMockito.mockStatic(MDC.class);
     Hashtable hashtable = new Hashtable();
@@ -36,13 +40,60 @@ public class Log4jMDCAdapterTest {
     Map map = log4jMDCAdapter.getCopyOfContextMap();
 
     //then
-    Assert.assertEquals(new HashMap(hashtable), map);
-
-    //TODO WIP add both cases--  like null and not null
+    assertEquals(new HashMap(hashtable), map);
   }
 
   @Test
-  public void testSetContextMap() throws Exception {
+  public void testGetCopyOfContextMap_withContextAsNull() throws Exception {
+    //setup
+    PowerMockito.mockStatic(MDC.class);
+    PowerMockito.when(MDC.class, "getContext").thenReturn(null);
 
+    //when
+    Map map = log4jMDCAdapter.getCopyOfContextMap();
+
+    //then
+    assertEquals(null, map);
+  }
+
+  @Test
+  public void testSetContextMap_withOldContextNotNull() throws Exception {
+    //setup
+    PowerMockito.mockStatic(MDC.class);
+    Hashtable<String,String> mdcContext= new Hashtable<String, String>();
+    mdcContext.put("aKey", "aVal");
+    mdcContext = Mockito.spy(mdcContext);
+    PowerMockito.when(MDC.class, "getContext").thenReturn(mdcContext);
+
+    Map<String, String> contextMap = new HashMap<String, String>();
+    contextMap.put("newKey", "newVal");
+
+    //when
+    log4jMDCAdapter.setContextMap(contextMap);
+
+    //then
+    Mockito.verify(mdcContext).clear();
+    Mockito.verify(mdcContext).putAll(contextMap);
+  }
+
+  @Test
+  public void testSetContextMap_withOldContextAsNull() throws Exception {
+    //setup
+    PowerMockito.mockStatic(MDC.class);
+    Hashtable<String,String> mdcContext= null;
+    PowerMockito.when(MDC.class, "getContext").thenReturn(mdcContext);
+
+    Map<String, String> contextMap = new HashMap<String, String>();
+    contextMap.put("newKey1", "newVal1");
+    contextMap.put("newKey2", "newVal2");
+
+    //when
+    log4jMDCAdapter.setContextMap(contextMap);
+
+    //then
+    PowerMockito.verifyStatic();
+    MDC.put("newKey1", "newVal1");
+    PowerMockito.verifyStatic();
+    MDC.put("newKey2", "newVal2");
   }
 }
