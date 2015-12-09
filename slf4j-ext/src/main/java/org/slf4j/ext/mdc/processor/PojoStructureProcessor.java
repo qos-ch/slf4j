7bf60154dcafcd8296ad62ab4fbe98c708314466
Khushali Modi <km021192@gmail.com>
2015-12-09 19:00:46 +0530

    Completed the logic in anno processor to generate the Java class representing tree structure by analysing the POJO

--- slf4j-ext/src/main/java/org/slf4j/ext/mdc/processor/PojoStructureProcessor.java
+++ slf4j-ext/src/main/java/org/slf4j/ext/mdc/processor/PojoStructureProcessor.java
@@ -4,7 +4,8 @@ package org.slf4j.ext.mdc.processor;
 import org.slf4j.ext.mdc.annotation.Pojo;
 import org.slf4j.ext.mdc.annotation.Property;
 import org.slf4j.ext.mdc.annotation.RootPojo;
-import org.slf4j.ext.mdc.simpletree.Node;
+import org.slf4j.ext.mdc.tree.Node;
+import org.slf4j.ext.mdc.tree.NonLeafNode;
 import org.slf4j.ext.mdc.tree.RootNode;
 
 import com.google.auto.service.AutoService;
@@ -79,9 +80,6 @@ public class PojoStructureProcessor extends AbstractProcessor{
       }
     }
     try {
-      System.out.println(rootElement.getSimpleName() + "--> Root");
-      rootNode = new Node(rootElement.getSimpleName().toString(),null);
-      System.out.println(rootElement);
       generateTreeCode(rootElement, elementUtils, filer);
     } catch (ProcessingException e) {
       error(e.getElement(), "Annotation processor error");
@@ -89,6 +87,134 @@ public class PojoStructureProcessor extends AbstractProcessor{
       e.printStackTrace();
     }
     return true;
+  }
+
+  private void generateTreeCode(Element root, Elements elementUtils, Filer filer) throws IOException, ProcessingException {
+    TypeElement classNameElement = elementUtils.getTypeElement(root.asType().toString());
+    String newClassName = classNameElement.getSimpleName().toString();
+    PackageElement pkg = elementUtils.getPackageOf(classNameElement);
+    String pkgName = pkg.isUnnamed()? null: pkg.getQualifiedName().toString();
+    MethodSpec.Builder constructorBuilder = MethodSpec.constructorBuilder().addModifiers(Modifier.PRIVATE);
+    constructorBuilder.addStatement("super(\"" + classNameElement.getSimpleName().toString().toUpperCase() + "\",\"" + classNameElement.getSimpleName().toString().toLowerCase() + "\")");
+    MethodSpec.Builder copyMethodBuilder = MethodSpec.methodBuilder("copy")
+            .addModifiers(Modifier.PUBLIC)
+            .addAnnotation(Override.class)
+            .returns(TypeName.get(root.asType()))
+            .addStatement(root.getSimpleName().toString() + " copy = new " + root.getSimpleName().toString() + "()");
+
+    ClassName parClass = ClassName.get(pkgName, newClassName);
+    ClassName rootNodeClass = ClassName.get("org.slf4j.ext.mdc.tree", "RootNode");
+    TypeName superRootNode = ParameterizedTypeName.get(rootNodeClass, parClass);
+
+    TypeSpec.Builder typeSpecBuilder = TypeSpec.classBuilder(newClassName)
+            .superclass(superRootNode)
+            .addMethod(constructorBuilder.build());
+
+    MethodSpec.Builder setDefaultMethodBuilder = MethodSpec.methodBuilder("setToDefault")
+            .addModifiers(Modifier.PUBLIC)
+            .addAnnotation(Override.class);
+
+    generateClassTree(root, typeSpecBuilder, copyMethodBuilder, setDefaultMethodBuilder);
+
+    typeSpecBuilder.addMethod(MethodSpec.methodBuilder("newInstance")
+                                      .addModifiers(Modifier.PROTECTED, Modifier.STATIC)
+                                      .returns(RootNode.class)
+                                      .addStatement("return new " + root.getSimpleName().toString() + "()")
+                                      .build());
+
+    typeSpecBuilder.addMethod(MethodSpec.methodBuilder("get")
+                                      .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                                      .returns(TypeName.get(root.asType()))
+                                      .addStatement("return (" + root.getSimpleName().toString() + ")RootNode.get()")
+                                      .build());
+
+
+    typeSpecBuilder.addMethod(setDefaultMethodBuilder.build());
+    typeSpecBuilder.addMethod(copyMethodBuilder.build());
+    JavaFile.builder(pkgName, typeSpecBuilder.build())
+            .build()
+            .writeTo(filer);
+
+  }
+
+  private void generateClassTree(Element root, TypeSpec.Builder typeSpecBuilder,
+                            MethodSpec.Builder copyMethodBuilder, MethodSpec.Builder setDefaultMethodBuilder ) throws ProcessingException {
+    for(Element e :root.getEnclosedElements()){
+      if(isProperty(e)){
+        Element e1 = ((DeclaredType)e.asType()).asElement();
+        if(isPojoType(e1)){
+          typeSpecBuilder.addField(FieldSpec.builder(TypeName.get(e.asType()), e.getSimpleName().toString(), Modifier.PRIVATE)
+                                           .initializer("new $T($S,this)", TypeName.get(e.asType()), e.getSimpleName().toString())
+                                           .build());
+          typeSpecBuilder.addMethod(MethodSpec.methodBuilder("get" + toProperCase(e.getSimpleName().toString()))
+                                            .addModifiers(Modifier.PUBLIC)
+                                            .returns(TypeName.get(e.asType()))
+                                            .addStatement("return this." + e.getSimpleName().toString())
+                                            .build());
+
+          typeSpecBuilder.addMethod(MethodSpec.methodBuilder("set" + toProperCase(e.getSimpleName().toString()))
+                                            .addModifiers(Modifier.PUBLIC)
+                                            .addParameter(TypeName.get(e.asType()), e.getSimpleName().toString())
+                                            .addStatement("this." + e.getSimpleName().toString() + " = " + e.getSimpleName().toString())
+                                            .build());
+
+          copyMethodBuilder.addStatement("copy.set" + toProperCase(e.getSimpleName().toString()) + "(" + e.getSimpleName().toString() + ".copy())");
+          setDefaultMethodBuilder.addStatement(e.getSimpleName().toString()+".setToDefault()");
+        } else if (isRootElement(e1)){
+          throw new ProcessingException((TypeElement)e1, " Only one root is allowed.");
+        } else {
+          String eleType  = e.asType().toString();
+          String literal = (eleType.contains("String")) ? "$S" : ((eleType.contains("Boolean")) ? "$N" : "$L");
+          typeSpecBuilder.addField(FieldSpec.builder(getElementToNodeType(e), e.getSimpleName().toString(), Modifier.PRIVATE)
+                                           .initializer("new $T($S, this, " + literal + ")"
+                                                   , getElementToNodeType(e), e.getSimpleName().toString()
+                                                   , getElementDefaultValue(e))
+                                           .build());
+
+          typeSpecBuilder.addMethod(MethodSpec.methodBuilder("get" + toProperCase(e.getSimpleName().toString()))
+                                            .addModifiers(Modifier.PUBLIC)
+                                            .returns(getElementToNodeType(e))
+                                            .addStatement("return this." + e.getSimpleName().toString())
+                                            .build());
+
+          typeSpecBuilder.addMethod(MethodSpec.methodBuilder("set" + toProperCase(e.getSimpleName().toString()))
+                                            .addModifiers(Modifier.PUBLIC)
+                                            .addParameter(getElementToNodeType(e), e.getSimpleName().toString())
+                                            .addStatement("this." + e.getSimpleName().toString() + " = " + e.getSimpleName().toString())
+                                            .build());
+
+          copyMethodBuilder.addStatement("copy.set" + toProperCase(e.getSimpleName().toString()) + "(this."+ e.getSimpleName().toString()+")");
+        }
+      } else if (isPojo(e)){
+        ClassName parClass = ClassName.get("org.slf4j.ext.mdc.example2", e.getSimpleName().toString());
+        ClassName nonLeafNodeClass = ClassName.get("org.slf4j.ext.mdc.tree", "NonLeafNode");
+        TypeName nonLeafNode = ParameterizedTypeName.get(nonLeafNodeClass, parClass);
+        MethodSpec.Builder innerConstructorBuilder = MethodSpec.constructorBuilder().addModifiers(Modifier.PRIVATE);
+        innerConstructorBuilder.addParameter(String.class, "name")
+                .addParameter(Node.class, "parent")
+                .addStatement("super(name, parent)");
+
+        MethodSpec.Builder innerSetDefaultMethodBuilder = MethodSpec.methodBuilder("setToDefault")
+                .addModifiers(Modifier.PUBLIC)
+                .addAnnotation(Override.class);
+        MethodSpec.Builder innerCopyMethodBuilder = MethodSpec.methodBuilder("copy")
+                .addParameter(Node.class, "parent")
+                .addModifiers(Modifier.PUBLIC)
+                .addAnnotation(Override.class)
+                .addStatement(e.getSimpleName() + " copy = new " + e.getSimpleName() + "(this.NAME, null)")
+                .addStatement("copy.setParent(parent)")
+                .returns(TypeName.get(e.asType()));
+        TypeSpec.Builder innerTypeSpecBuilder = TypeSpec.classBuilder(e.getSimpleName().toString())
+                .addMethod(innerConstructorBuilder.build())
+                .addModifiers(Modifier.PUBLIC)
+                .superclass(nonLeafNode);
+        generateClassTree(e, innerTypeSpecBuilder, innerCopyMethodBuilder, innerSetDefaultMethodBuilder);
+        innerTypeSpecBuilder.addMethod(innerSetDefaultMethodBuilder.build())
+                .addMethod(innerCopyMethodBuilder.build());
+        typeSpecBuilder.addType(innerTypeSpecBuilder.build());
+      }
+    }
+    copyMethodBuilder.addStatement("return copy");
   }
 
   private boolean isRootElement(Element rootElement){
@@ -99,7 +225,14 @@ public class PojoStructureProcessor extends AbstractProcessor{
   }
 
   private boolean isPojo(Element element){
-    if(((DeclaredType)element.asType()).asElement().getAnnotation(Pojo.class) != null){
+    if((element.getAnnotation(Pojo.class) != null)){
+      return true;
+    }
+    return false;
+  }
+
+  private boolean isPojoType(Element element){
+    if(((DeclaredType)(element.asType())).asElement().getAnnotation(Pojo.class) != null){
       return true;
     }
     return false;
@@ -112,79 +245,8 @@ public class PojoStructureProcessor extends AbstractProcessor{
     return false;
   }
 
-  private void generateTreeCode(Element root, Elements elementUtils, Filer filer) throws IOException, ProcessingException {
-    TypeElement classNameElement = elementUtils.getTypeElement(root.asType().toString());
-    String newClassName = classNameElement.getSimpleName().toString();
-    PackageElement pkg = elementUtils.getPackageOf(classNameElement);
-    String pkgName = pkg.isUnnamed()? null: pkg.getQualifiedName().toString();
-    MethodSpec.Builder constructorBuilder = MethodSpec.constructorBuilder().addModifiers(Modifier.PRIVATE);
-    constructorBuilder.addStatement("super(\"" + classNameElement.getSimpleName().toString().toUpperCase() + "\",\"" + classNameElement.getSimpleName().toString().toLowerCase() + "\")");
-
-    TypeSpec.Builder typeSpecBuilder = TypeSpec.classBuilder(newClassName)
-            .superclass(RootNode.class)
-            .addMethod(constructorBuilder.build());
-
-    for(Element e :root.getEnclosedElements()){
-      if(isProperty(e)){
-        Element e1 = ((DeclaredType)e.asType()).asElement();
-        if(isPojo(e1)){
-          typeSpecBuilder.addField(FieldSpec.builder(TypeName.get(e.asType()), e.getSimpleName().toString(), Modifier.PRIVATE)
-                                           .initializer("new $T($S,this)", TypeName.get(e.asType()), e.getSimpleName().toString())
-                                           .build());
-          typeSpecBuilder.addMethod(MethodSpec.methodBuilder("get" + toCamelCase(e.getSimpleName().toString()))
-                                            .addModifiers(Modifier.PUBLIC)
-                                            .returns(TypeName.get(e.asType()))
-                                            .addStatement("return this." + e.getSimpleName().toString())
-                                            .build());
-
-          typeSpecBuilder.addMethod(MethodSpec.methodBuilder("set" + toCamelCase(e.getSimpleName().toString()))
-                                            .addModifiers(Modifier.PUBLIC)
-                                            .addParameter(TypeName.get(e.asType()), e.getSimpleName().toString())
-                                            .addStatement("this." + e.getSimpleName().toString() + " = "+ e.getSimpleName().toString())
-                                            .build());
-
-
-        } else if (isRootElement(e1)){
-          throw new ProcessingException((TypeElement)e1, " Only one root is allowed.");
-        } else {
-          typeSpecBuilder.addField(FieldSpec.builder(getElementToNodeType(e), e.getSimpleName().toString(), Modifier.PRIVATE)
-                                           .initializer("new $T($S, this, $S)", getElementToNodeType(e), e.getSimpleName().toString(), getElementDefaultValue(e))
-                                           .build());
-
-          typeSpecBuilder.addMethod(MethodSpec.methodBuilder("get" + toCamelCase(e.getSimpleName().toString()))
-                                            .addModifiers(Modifier.PUBLIC)
-                                            .returns(getElementToNodeType(e))
-                                            .addStatement("return this." + e.getSimpleName().toString())
-                                            .build());
-
-          typeSpecBuilder.addMethod(MethodSpec.methodBuilder("set" + toCamelCase(e.getSimpleName().toString()))
-                                            .addModifiers(Modifier.PUBLIC)
-                                            .addParameter(getElementToNodeType(e), e.getSimpleName().toString())
-                                            .addStatement("this." + e.getSimpleName().toString() + " = "+ e.getSimpleName().toString())
-                                            .build());
-
-
-        }
-      }
-    }
-    JavaFile.builder(pkgName, typeSpecBuilder.build())
-            .build()
-            .writeTo(filer);
-
-  }
-
-  public static String toCamelCase(String s){
-    String[] parts = s.split("_");
-    String camelCaseString = "";
-    for (String part : parts){
-      camelCaseString = camelCaseString + toProperCase(part);
-    }
-    return camelCaseString;
-  }
-
   public static String toProperCase(String s) {
-    return s.substring(0, 1).toUpperCase() +
-            s.substring(1).toLowerCase();
+    return s.substring(0, 1).toUpperCase() + s.substring(1);
   }
 
   private ClassName getElementToNodeType(Element e){
@@ -192,21 +254,23 @@ public class PojoStructureProcessor extends AbstractProcessor{
     String pkg = "org.slf4j.ext.mdc.tree";
     if(e.asType().toString().contains("String")){
       entityClassName = ClassName.get(pkg, "StringNode");
-    } else if(e.asType().toString().contains("boolean")){
+    } else if(e.asType().toString().contains("Boolean")){
       entityClassName = ClassName.get(pkg, "BooleanNode");
-    } else if(e.asType().toString().contains("int")){
+    } else if(e.asType().toString().contains("int") ||
+            e.asType().toString().contains("Number")){
       entityClassName = ClassName.get(pkg, "IntegerNode");
     }
     return entityClassName;
   }
 
   private String getElementDefaultValue(Element e){
-    String pkg = "org.slf4j.ext.mdc.tree";
     if(e.asType().toString().contains("String")){
-      return "0";
-    } else if(e.asType().toString().contains("boolean")){
+      return "";
+    } else if(e.asType().toString().contains("Boolean")){
       return "true";
-    } else if(e.asType().toString().contains("int")){
+    } else if(e.asType().toString().contains("Number")){
+      return "0";
+    }else if(e.asType().toString().contains("int")){
       return "0";
     }
     return null;
@@ -220,5 +284,13 @@ public class PojoStructureProcessor extends AbstractProcessor{
 
   public void error(Element e, String msg){
     messsager.printMessage(Diagnostic.Kind.ERROR, msg, e);
+  }
+
+  public static class DefaultValues{
+    public static final String defaultString = "";
+    public static final Integer defaultInteger = 0;
+    public static final Number defaultNumber = 0;
+    public static final Boolean defaultBoolean = true;
+
   }
 }
