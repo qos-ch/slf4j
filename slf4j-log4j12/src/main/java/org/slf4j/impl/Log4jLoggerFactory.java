dbe2d7271939a2e7f3197fd4ca5ce399e563dd31
Frans Orsel <frans.orsel@klm.com>
2015-12-15 17:26:35 +0100

    Fix ordering of import and static variable/method

--- slf4j-log4j12/src/main/java/org/slf4j/impl/Log4jLoggerFactory.java
+++ slf4j-log4j12/src/main/java/org/slf4j/impl/Log4jLoggerFactory.java
@@ -28,9 +28,9 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 import org.apache.log4j.LogManager;
+import org.slf4j.helpers.Util;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
-import org.slf4j.helpers.Util;
 
 /**
  * Log4jLoggerFactory is an implementation of {@link ILoggerFactory} returning
@@ -39,9 +39,6 @@ import org.slf4j.helpers.Util;
  * @author Ceki G&uuml;lc&uuml;
  */
 public class Log4jLoggerFactory implements ILoggerFactory {
-
-    // key: name (String), value: a Log4jLoggerAdapter;
-    ConcurrentMap<String, Logger> loggerMap;
 
     private static final String LOG4J_DELEGATION_LOOP_URL = "http://www.slf4j.org/codes.html#log4jDelegationLoop";
 	
@@ -59,6 +56,9 @@ public class Log4jLoggerFactory implements ILoggerFactory {
             // this is the good case
         }
     }
+
+    // key: name (String), value: a Log4jLoggerAdapter;
+    ConcurrentMap<String, Logger> loggerMap;
 
     public Log4jLoggerFactory() {
         loggerMap = new ConcurrentHashMap<String, Logger>();
