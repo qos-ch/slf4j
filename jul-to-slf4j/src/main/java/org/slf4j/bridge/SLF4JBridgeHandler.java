29922a48d1376cd1d0a5f8a0ef0d23d4bd563609
Paul del Perugia <p.delperugia@gmail.com>
2016-02-15 18:12:17 +0100

    SLF4JBridgeHandler : bug fix Null Message problem. The message was checked if null but never updated to empty string in the record.

--- jul-to-slf4j/src/main/java/org/slf4j/bridge/SLF4JBridgeHandler.java
+++ jul-to-slf4j/src/main/java/org/slf4j/bridge/SLF4JBridgeHandler.java
@@ -298,6 +298,7 @@ public class SLF4JBridgeHandler extends Handler {
         // see also http://jira.qos.ch/browse/SLF4J-99
         if (message == null) {
             message = "";
+            record.setMessage(message);
         }
         if (slf4jLogger instanceof LocationAwareLogger) {
             callLocationAwareLogger((LocationAwareLogger) slf4jLogger, record);
