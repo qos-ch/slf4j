df2a37d333b2796e7c158d0410027f1fc33de14b
Ceki Gulcu <ceki@qos.ch>
2016-03-01 01:23:08 +0100

    remove Jigloo license header

--- slf4j-migrator/src/main/java/org/slf4j/migrator/internal/MigratorFrame.java
+++ slf4j-migrator/src/main/java/org/slf4j/migrator/internal/MigratorFrame.java
@@ -46,16 +46,6 @@ import javax.swing.WindowConstants;
 import org.slf4j.migrator.Constant;
 import org.slf4j.migrator.helper.SpringLayoutHelper;
 
-/**
- * This code was edited or generated using CloudGarden's Jigloo SWT/Swing GUI
- * Builder, which is free for non-commercial use. If Jigloo is being used
- * commercially (ie, by a corporation, company or business for any purpose
- * whatever) then you should purchase a license for each developer using Jigloo.
- * Please visit www.cloudgarden.com for details. Use of Jigloo implies
- * acceptance of these licensing terms. A COMMERCIAL LICENSE HAS NOT BEEN
- * PURCHASED FOR THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED LEGALLY FOR
- * ANY CORPORATE OR COMMERCIAL PURPOSE.
- */
 public class MigratorFrame extends JFrame implements ActionListener {
     private static final long serialVersionUID = 1L;
 
