8bf1f8cf5ca1bc9df8cedd63d8685ab961110113
Ceki Gulcu <ceki@qos.ch>
2016-03-28 23:36:33 +0200

    add adds on right panel

--- slf4j-site/src/site/pages/templates/left.js
+++ slf4j-site/src/site/pages/templates/left.js
@@ -56,4 +56,13 @@ document.write('<div class="pub"><img src="https://travis-ci.org/qos-ch/slf4j.sv
 //document.write('</p>');
 
 
-
+//document.write('<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
+//<!-- SLF4J -->
+//document.write('<ins class="adsbygoogle"');
+//document.write('     style="display:block"');
+//document.write('     data-ad-client="ca-pub-7471410671306824"');
+//document.write('     data-ad-slot="7413932813"');
+//document.write('     data-ad-format="auto"></ins>');
+//document.write('<script>');
+//document.write('(adsbygoogle = window.adsbygoogle || []).push({});');
+//document.write('</script>');
--- slf4j-site/src/site/pages/templates/right.js
+++ slf4j-site/src/site/pages/templates/right.js
@@ -1,1 +1,10 @@
-
+document.write('      <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
+//      <!-- SLF4J -->
+document.write('      <ins class="adsbygoogle"');
+document.write('           style="display:block"');
+document.write('           data-ad-client="ca-pub-7471410671306824"');
+document.write('           data-ad-slot="7413932813"');
+document.write('           data-ad-format="auto"></ins>');
+document.write('      <script>');
+document.write('        (adsbygoogle = window.adsbygoogle || []).push({});');
+document.write('      </script>;');
