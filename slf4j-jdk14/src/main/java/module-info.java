3823bd18e8829e72bf4a63cca62eb6c27e9132b6
Ceki Gulcu <ceki@qos.ch>
2017-04-25 23:35:40 +0200

    no need to export 'org.slf4j.jul' package

--- slf4j-jdk14/src/main/java/module-info.java
+++ slf4j-jdk14/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module org.slf4j.jul { 
   requires org.slf4j;
   requires java.logging;
-  exports org.slf4j.jul;
   provides org.slf4j.spi.SLF4JServiceProvider with org.slf4j.jul.JULServiceProvider;
 }
 
