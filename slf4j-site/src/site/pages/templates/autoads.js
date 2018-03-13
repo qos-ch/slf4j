0ec1f6aac8648e87a7dda2f5730fef6db3b4fa33
Ceki Gulcu <ceki@qos.ch>
2018-03-13 16:17:59 +0100

    remove autoads.js

--- slf4j-site/src/site/pages/templates/autoads.js
+++
@@ -1,9 +0,0 @@
-
-
-document.write('<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
-document.write('<script>');
-document.write('     (adsbygoogle = window.adsbygoogle || []).push({');
-document.write('          google_ad_client: "ca-pub-7471410671306824",');
-document.write('          enable_page_level_ads: true');
-document.write('     });');
-document.write('</script>');
