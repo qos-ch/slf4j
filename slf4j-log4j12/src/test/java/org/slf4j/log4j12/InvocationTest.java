6a09a76bb6930ceb7fd436169e15140fb9b393f3
Ceki Gulcu <ceki@qos.ch>
2021-06-15 23:03:21 +0200

    rename private method

--- slf4j-log4j12/src/test/java/org/slf4j/log4j12/InvocationTest.java
+++ slf4j-log4j12/src/test/java/org/slf4j/log4j12/InvocationTest.java
@@ -27,6 +27,7 @@ package org.slf4j.log4j12;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
 
 import java.util.HashMap;
@@ -50,141 +51,160 @@ import org.slf4j.MarkerFactory;
  */
 public class InvocationTest {
 
-    ListAppender listAppender = new ListAppender();
-    org.apache.log4j.Logger root;
+	ListAppender listAppender = new ListAppender();
+	org.apache.log4j.Logger root;
 
-    @Before
-    public void setUp() throws Exception {
-        root = org.apache.log4j.Logger.getRootLogger();
-        root.addAppender(listAppender);
-    }
+	@Before
+	public void setUp() throws Exception {
+		root = org.apache.log4j.Logger.getRootLogger();
+		root.addAppender(listAppender);
+	}
 
-    @After
-    public void tearDown() throws Exception {
-        root.getLoggerRepository().resetConfiguration();
-    }
+	@After
+	public void tearDown() throws Exception {
+		root.getLoggerRepository().resetConfiguration();
+	}
 
-    @Test
-    public void test1() {
-        Logger logger = LoggerFactory.getLogger("test1");
-        logger.debug("Hello world.");
-        assertEquals(1, listAppender.list.size());
-    }
+	@Test
+	public void test1() {
 
-    @Test
-    public void test2() {
-        Integer i1 = Integer.valueOf(1);
-        Integer i2 = Integer.valueOf(2);
-        Integer i3 = Integer.valueOf(3);
-        Exception e = new Exception("This is a test exception.");
-        Logger logger = LoggerFactory.getLogger("test2");
+		Logger logger = LoggerFactory.getLogger("test1");
+		logger.debug("Hello world.");
+		assertEquals(1, listAppender.list.size());
+	}
 
-        logger.trace("Hello trace.");
+	@Test
+	public void test2() {
+		Integer i1 = Integer.valueOf(1);
+		Integer i2 = Integer.valueOf(2);
+		Integer i3 = Integer.valueOf(3);
+		Exception e = new Exception("This is a test exception.");
+		Logger logger = LoggerFactory.getLogger("test2");
 
-        logger.debug("Hello world 1.");
-        logger.debug("Hello world {}", i1);
-        logger.debug("val={} val={}", i1, i2);
-        logger.debug("val={} val={} val={}", new Object[] { i1, i2, i3 });
+		logger.trace("Hello trace.");
 
-        logger.debug("Hello world 2", e);
-        logger.info("Hello world 2.");
+		logger.debug("Hello world 1.");
+		logger.debug("Hello world {}", i1);
+		logger.debug("val={} val={}", i1, i2);
+		logger.debug("val={} val={} val={}", new Object[] { i1, i2, i3 });
 
-        logger.warn("Hello world 3.");
-        logger.warn("Hello world 3", e);
+		logger.debug("Hello world 2", e);
+		logger.info("Hello world 2.");
 
-        logger.error("Hello world 4.");
-        logger.error("Hello world {}", Integer.valueOf(3));
-        logger.error("Hello world 4.", e);
-        assertEquals(11, listAppender.list.size());
-    }
+		logger.warn("Hello world 3.");
+		logger.warn("Hello world 3", e);
 
-    @Test
-    public void testNull() {
-        Logger logger = LoggerFactory.getLogger("testNull");
-        logger.trace(null);
-        logger.debug(null);
-        logger.info(null);
-        logger.warn(null);
-        logger.error(null);
+		logger.error("Hello world 4.");
+		logger.error("Hello world {}", Integer.valueOf(3));
+		logger.error("Hello world 4.", e);
+		assertEquals(11, listAppender.list.size());
+	}
 
-        Exception e = new Exception("This is a test exception.");
-        logger.debug(null, e);
-        logger.info(null, e);
-        logger.warn(null, e);
-        logger.error(null, e);
-        assertEquals(8, listAppender.list.size());
-    }
+	@Test
+	public void testNull() {
+		Logger logger = LoggerFactory.getLogger("testNull");
+		logger.trace(null);
+		logger.debug(null);
+		logger.info(null);
+		logger.warn(null);
+		logger.error(null);
 
-    // http://jira.qos.ch/browse/SLF4J-69
-    // formerly http://bugzilla.slf4j.org/show_bug.cgi?id=78
-    @Test
-    public void testNullParameter_BUG78() {
-        Logger logger = LoggerFactory.getLogger("testNullParameter_BUG78");
-        String[] parameters = null;
-        String msg = "hello {}";
+		Exception e = new Exception("This is a test exception.");
+		logger.debug(null, e);
+		logger.info(null, e);
+		logger.warn(null, e);
+		logger.error(null, e);
+		assertEquals(8, listAppender.list.size());
+	}
 
-        logger.debug(msg, (Object[]) parameters);
+	// http://jira.qos.ch/browse/SLF4J-69
+	// formerly http://bugzilla.slf4j.org/show_bug.cgi?id=78
+	@Test
+	public void testNullParameter_BUG78() {
+		Logger logger = LoggerFactory.getLogger("testNullParameter_BUG78");
+		String[] parameters = null;
+		String msg = "hello {}";
 
-        assertEquals(1, listAppender.list.size());
-        LoggingEvent e = (LoggingEvent) listAppender.list.get(0);
-        assertEquals(msg, e.getMessage());
-    }
+		logger.debug(msg, (Object[]) parameters);
 
-    @Test
-    public void testMarker() {
-        Logger logger = LoggerFactory.getLogger("testMarker");
-        Marker blue = MarkerFactory.getMarker("BLUE");
-        logger.trace(blue, "hello");
-        logger.debug(blue, "hello");
-        logger.info(blue, "hello");
-        logger.warn(blue, "hello");
-        logger.error(blue, "hello");
+		assertEquals(1, listAppender.list.size());
+		LoggingEvent e = (LoggingEvent) listAppender.list.get(0);
+		assertEquals(msg, e.getMessage());
+	}
 
-        logger.debug(blue, "hello {}", "world");
-        logger.info(blue, "hello {}", "world");
-        logger.warn(blue, "hello {}", "world");
-        logger.error(blue, "hello {}", "world");
+	@Test
+	public void testMarker() {
+		Logger logger = LoggerFactory.getLogger("testMarker");
+		Marker blue = MarkerFactory.getMarker("BLUE");
+		logger.trace(blue, "hello");
+		logger.debug(blue, "hello");
+		logger.info(blue, "hello");
+		logger.warn(blue, "hello");
+		logger.error(blue, "hello");
 
-        logger.debug(blue, "hello {} and {} ", "world", "universe");
-        logger.info(blue, "hello {} and {} ", "world", "universe");
-        logger.warn(blue, "hello {} and {} ", "world", "universe");
-        logger.error(blue, "hello {} and {} ", "world", "universe");
-        assertEquals(12, listAppender.list.size());
-    }
+		logger.debug(blue, "hello {}", "world");
+		logger.info(blue, "hello {}", "world");
+		logger.warn(blue, "hello {}", "world");
+		logger.error(blue, "hello {}", "world");
 
-    @Test
-    public void testMDC() {
-        MDC.put("k", "v");
-        assertNotNull(MDC.get("k"));
-        assertEquals("v", MDC.get("k"));
+		logger.debug(blue, "hello {} and {} ", "world", "universe");
+		logger.info(blue, "hello {} and {} ", "world", "universe");
+		logger.warn(blue, "hello {} and {} ", "world", "universe");
+		logger.error(blue, "hello {} and {} ", "world", "universe");
+		assertEquals(12, listAppender.list.size());
+	}
 
-        MDC.remove("k");
-        assertNull(MDC.get("k"));
+	@Test
+	public void testMDC() {
+		MDC.put("k", "v");
+		assertNotNull(MDC.get("k"));
+		assertEquals("v", MDC.get("k"));
 
-        MDC.put("k1", "v1");
-        assertEquals("v1", MDC.get("k1"));
-        MDC.clear();
-        assertNull(MDC.get("k1"));
+		MDC.remove("k");
+		assertNull(MDC.get("k"));
 
-        try {
-            MDC.put(null, "x");
-            fail("null keys are invalid");
-        } catch (IllegalArgumentException e) {
-        }
-    }
+		MDC.put("k1", "v1");
+		assertEquals("v1", MDC.get("k1"));
+		MDC.clear();
+		assertNull(MDC.get("k1"));
 
-    @Test
-    public void testMDCContextMapValues() {
-        Map<String, String> map = new HashMap<String, String>();
-        map.put("ka", "va");
-        map.put("kb", "vb");
+		try {
+			MDC.put(null, "x");
+			fail("null keys are invalid");
+		} catch (IllegalArgumentException e) {
+		}
+	}
 
-        MDC.put("k", "v");
-        assertEquals("v", MDC.get("k"));
-        MDC.setContextMap(map);
-        assertNull(MDC.get("k"));
-        assertEquals("va", MDC.get("ka"));
-        assertEquals("vb", MDC.get("kb"));
-    }
+	@Test
+	public void testMDCContextMapValues() {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("ka", "va");
+		map.put("kb", "vb");
+
+		MDC.put("k", "v");
+		assertEquals("v", MDC.get("k"));
+		MDC.setContextMap(map);
+		assertNull(MDC.get("k"));
+		assertEquals("va", MDC.get("ka"));
+		assertEquals("vb", MDC.get("kb"));
+	}
+
+	@Test
+	public void testCallerInfo() {
+		Logger logger = LoggerFactory.getLogger("testMarker");
+		listAppender.extractLocationInfo = true;
+		logger.debug("hello");
+		LoggingEvent event = listAppender.list.get(0);
+		assertEquals(this.getClass().getName(), event.getLocationInformation().getClassName());
+	}
+
+	@Test
+	public void testCallerInfoWithFluentAPI() {
+		Logger logger = LoggerFactory.getLogger("testMarker");
+		listAppender.extractLocationInfo = true;
+		logger.atDebug().log("hello");
+		LoggingEvent event = listAppender.list.get(0);
+		assertEquals(this.getClass().getName(), event.getLocationInformation().getClassName());
+	}
 
 }
