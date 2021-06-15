2d1d948c5e25b8fbd13f6b646f77971e7e3a2fef
Ceki Gulcu <ceki@qos.ch>
2021-06-15 23:51:03 +0200

    fix SLF4J-511

--- slf4j-api/src/main/java/org/slf4j/spi/LoggingEventAware.java
+++ slf4j-api/src/main/java/org/slf4j/spi/LoggingEventAware.java
@@ -2,8 +2,12 @@ package org.slf4j.spi;
 
 import org.slf4j.event.LoggingEvent;
 
+/**
+ * A logger capable of logging from org.slf4j.event.LoggingEvent implements this interface.
+ * 
+ * @author Ceki Gulcu
+ * @since 2.0.0
+ */
 public interface LoggingEventAware {
-
 	void log(LoggingEvent event);
-	
 }
