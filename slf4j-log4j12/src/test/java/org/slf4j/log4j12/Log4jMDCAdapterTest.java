ff4a3a438ef9a10898f80dd0242d8f93cb24bdd4
Ceki Gulcu <ceki@qos.ch>
2021-07-02 16:23:17 +0200

    fix unit tests
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- slf4j-log4j12/src/test/java/org/slf4j/log4j12/Log4jMDCAdapterTest.java
+++ slf4j-log4j12/src/test/java/org/slf4j/log4j12/Log4jMDCAdapterTest.java
@@ -1,13 +1,28 @@
 package org.slf4j.log4j12;
 
-import org.slf4j.helpers.BasicMDCAdapterTest;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.slf4j.helpers.MDCAdapterTestBase;
 import org.slf4j.spi.MDCAdapter;
 
-public class Log4jMDCAdapterTest extends BasicMDCAdapterTest {
+public class Log4jMDCAdapterTest extends MDCAdapterTestBase {
     
     protected MDCAdapter instantiateMDC() {
         return new Log4jMDCAdapter();
     }
+    
+    
+    @Test
+    public void testClearingMDC() {
+        mdc.put("testKey", "testValue");
+        assertFalse(mdc.getCopyOfContextMap().isEmpty());
+        mdc.clear();
+        assertTrue(mdc.getCopyOfContextMap().isEmpty());
+    }
+
     
 
 }
