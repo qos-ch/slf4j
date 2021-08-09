280129046e53fd60e6743a57f04bbcfc91cbf3ce
Ceki Gulcu <ceki@qos.ch>
2021-08-09 15:11:43 +0200

    very minor changes
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- slf4j-log4j12/src/test/java/org/slf4j/log4j12/Log4jMDCAdapterTest.java
+++ slf4j-log4j12/src/test/java/org/slf4j/log4j12/Log4jMDCAdapterTest.java
@@ -1,7 +1,6 @@
 package org.slf4j.log4j12;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
