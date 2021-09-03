6f0c54caa4b339f285bf5e70d00741bc89c6ed4b
Ceki Gulcu <ceki@qos.ch>
2021-09-03 19:34:45 +0200

    add jquery to pages
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- slf4j-site/src/site/pages/templates/right.js
+++ slf4j-site/src/site/pages/templates/right.js
@@ -1,10 +1,40 @@
-document.write('      <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
+
+
+//$.getScript("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
+//   function() {
+//       jQuery("#right").append('<ins class="adsbygoogle" \
+//          style="display:block" \
+//          data-ad-client="ca-pub-7471410671306824" \
+//          data-ad-slot="7413932813" \
+//          data-ad-format="auto" \
+//          data-full-width-responsive="true"></ins> \
+//        <script> \
+//         (adsbygoogle = window.adsbygoogle || []).push({}); \
+//        </script>');
+//       }
+//  )
+//  .fail(
+//      function() {
+//          document.write("<p>Adblock detected</p>");
+//     }
+//  );
+//
+
+function addBlockDetected() {
+    jQuery("#right").append('<p class="big">If using SLF4J saves you hours of work, please whitelist this site in your \
+add blocker.</p>');
+}
+
+document.write('      <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" \
+  onerror="addBlockDetected();"  ></script>');
+
 //      <!-- SLF4J -->
-document.write('      <ins class="adsbygoogle"');
-document.write('           style="display:block"');
-document.write('           data-ad-client="ca-pub-7471410671306824"');
-document.write('           data-ad-slot="7413932813"');
-document.write('           data-ad-format="auto"></ins>');
-document.write('      <script>');
-document.write('        (adsbygoogle = window.adsbygoogle || []).push({});');
-document.write('      </script>;');
+jQuery("#right").append(' <ins class="adsbygoogle" \
+           style="display:block" \
+           data-ad-client="ca-pub-7471410671306824" \
+           data-ad-slot="7413932813" \
+           data-ad-format="auto"></ins> \
+      <script> \
+       (adsbygoogle = window.adsbygoogle || []).push({});\
+      </script>');
+
