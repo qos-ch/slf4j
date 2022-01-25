561738747da5813b8a9d2e2ebd10f0128a78a960
Ceki Gulcu <ceki@qos.ch>
2022-01-25 11:41:38 +0100

    add relase.sh
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- release.sh
+++ release.sh
@@ -1,49 +1,17 @@
-#mvn versions:set -DgenerateBackupPoms=false -DnewVersion=${VERSION_NUMBER} 
+mvn versions:set -DgenerateBackupPoms=false -DnewVersion=${VERSION_NUMBER} 
 
-MVN='/java/maven-3.5.2//bin/mvn'
+edit slf4j-log4j12 version 
 
-PASS=$1
-echo $PASS
+mvn clean
+mvn install
 
-function checkExit(){
-    if test "$?" != "0"; then
-      echo Command $1 exited with abnormal status
-      exit 1;
-    else echo $?
-    fi
-}
+mvn deploy -P javadocjar,sign-artifacts -Dgpg.passphrase=passwd
 
-function echoThenRun () { # echo and then run the command
-  echo $1
-  $1
-  ret=$?
-  echo $ret
-  return $ret
-}
+#uncomment diffie-hellman support in /etc/ssh/sshd_config
 
-$MVN clean
-checkExit "mvn clean"
+mvn site:deploy -N # with Java 8!!!
 
+git tag -m "tagging" -a v_${VERSION_NUMBER}
+git push --tags
 
-$MVN install
-checkExit "mvn install"
-
-
-$MVN site:site
-checkExit "mvn site:ste"
-
-
-$MVN assembly:single
-checkExit "mvn assembly:single"
-
-
-$MVN deploy -P javadocjar,sign-artifacts -Dgpg.passphrase=$PASS
-checkExit "mvn deploy -P javadocjar,sign-artifacts -Dgpg.passphrase=xxx"
-
-#$MVN site:deploy -N # with Java 8!!!
-#checkExit "mvn site:deploy -N"
-
-#git tag -m "tagging" -a v_${VERSION_NUMBER}
-#git push --tags
-
-#release version and add next version on jira
+release version and add next version on jira
