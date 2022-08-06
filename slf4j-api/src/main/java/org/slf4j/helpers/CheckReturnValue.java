1068cd0eb9fb2460d368e3ba6112517ef3fedd2a
Ceki Gulcu <ceki@qos.ch>
2022-08-06 23:25:49 +0200

    javadoc changes
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- slf4j-api/src/main/java/org/slf4j/helpers/CheckReturnValue.java
+++ slf4j-api/src/main/java/org/slf4j/helpers/CheckReturnValue.java
@@ -10,8 +10,14 @@ import java.lang.annotation.*;
  * which return an instance of LoggingEventBuilder (usually as <code>this</code>). Such
  * methods should be followed by one of the terminating <code>log()</code> methods returning
  * <code>void</code>.</p>
+ * <p></p>
+ * <p>IntelliJ IDEA supports a check for annotations named as <code>CheckReturnValue</code>
+ * regardless of the containing package. For more information on this feature in IntelliJ IDEA, select File &#8594; Setting &#8594;
+ * Editor &#8594; Inspections and then Java &#8594; Probable Bugs &#8594; Result of method call ignored. </p>
+ * <p></p>
  *
- * <p>Some IDEs such as IntelliJ IDEA support this annotation at compile time.</p>
+ * <p>As for Eclipse, this feature has been requested in
+ * <a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=572496">bug 572496</a></p>
  *
  * @author Ceki G&uuml;lc&uuml;
  * @since 2.0.0-beta1
