d81affb012f37e1d1eb8c2c6fbbb3a4ee15eb3e8
Ceki Gulcu <ceki@qos.ch>
2022-09-26 19:57:31 +0200

    comment about ThreadLocal key or value types
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- slf4j-api/src/main/java/org/slf4j/helpers/ThreadLocalMapOfStacks.java
+++ slf4j-api/src/main/java/org/slf4j/helpers/ThreadLocalMapOfStacks.java
@@ -15,6 +15,9 @@ import java.util.Map;
  */
 public class ThreadLocalMapOfStacks {
 
+    // BEWARE: Keys or values placed in a ThreadLocal should not be of a type/class
+    // not included in the JDK. See also https://jira.qos.ch/browse/LOGBACK-450
+
     final ThreadLocal<Map<String, Deque<String>>> tlMapOfStacks = new ThreadLocal<>();
 
     public void pushByKey(String key, String value) {
