a29e4b86296cb1858dbe04bbe7858b4dd1a2036f
Ceki Gulcu <ceki@qos.ch>
2024-02-05 22:39:28 +0100

    providers should open their main packages, allow org.slf4j reeflection
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- slf4j-jdk14/src/main/java9/module-info.java
+++ slf4j-jdk14/src/main/java9/module-info.java
@@ -5,8 +5,9 @@ module org.slf4j.jul {
   requires org.slf4j;
   requires java.logging;
   provides org.slf4j.spi.SLF4JServiceProvider with org.slf4j.jul.JULServiceProvider;
-  opens org.slf4j.jul to org.slf4j;
 
   exports org.slf4j.jul;
+  opens org.slf4j.jul to org.slf4j;
+
 }
 
--- slf4j-nop/src/main/java9/module-info.java
+++ slf4j-nop/src/main/java9/module-info.java
@@ -4,5 +4,6 @@
 module org.slf4j.nop {
   requires org.slf4j;
   exports org.slf4j.nop;
+  opens org.slf4j.nop to org.slf4j;
   provides org.slf4j.spi.SLF4JServiceProvider with org.slf4j.nop.NOPServiceProvider;
 }
