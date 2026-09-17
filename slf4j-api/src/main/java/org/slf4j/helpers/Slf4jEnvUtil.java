f059847490a619ff76c6154ba7164229db3b8df7
ceki <ceki@qos.ch>
2026-09-17 18:00:15 +0200

    update license year
    Signed-off-by: ceki <ceki@qos.ch>

--- slf4j-api/src/main/java/org/slf4j/helpers/Slf4jEnvUtil.java
+++ slf4j-api/src/main/java/org/slf4j/helpers/Slf4jEnvUtil.java
@@ -1,6 +1,6 @@
 /*
- *  Copyright (c) 2004-2024 QOS.ch
- *  All rights reserved.
+ * Copyright (C) 2004-2026, QOS.ch (Switzerland)
+ * All rights reserved.
  *
  *  Permission is hereby granted, free  of charge, to any person obtaining
  *  a  copy  of this  software  and  associated  documentation files  (the
